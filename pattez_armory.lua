347ed988be195b181b30feea2c946a73bad7b986
Patrik Olofsson <patrik.d.olofsson@gmail.com>
2019-11-09 17:30:10 +0100

    Adds enchantIds, changes mouseover to target

--- pattez_armory.lua
+++ pattez_armory.lua
@@ -5,25 +5,63 @@ if pattez_armory == nil then
   pattez_armory = {}
 end
 
+local lastGUID = false
+
 local f = CreateFrame("Frame")
-f:SetScript("OnEvent", function (_, event, args)
-  if event == "UPDATE_MOUSEOVER_UNIT" then
-    local inRange = CheckInteractDistance("mouseover", 3)
-    local enemy = UnitIsEnemy("mouseover", "mouseover")
-    if UnitExists("mouseover") and UnitIsPlayer("mouseover") and inRange and not enemy then
-    local name = UnitName("mouseover")
-    local level = UnitLevel("mouseover")
+f:SetScript("OnEvent", function (self, event, ...)
+	  self[event](self, ...)
+end)
+
+local inspectTarget
+local tryAgain = true
+
+MyAddOn = LibStub("AceAddon-3.0"):NewAddon("MyAddOn", "AceTimer-3.0")
+
+local function postHook(typeID, index)
+  lastGUID = UnitGUID(typeID)
+end
+
+hooksecurefunc("NotifyInspect", postHook);
+
+
+function MyAddOn:TimerFeedback()
+  if tryAgain then
+    inspectTarget = true
+    NotifyInspect("target")
+    MyAddOn:ScheduleTimer("TimerFeedback", 1)
+  end
+end
+
+function f:INSPECT_READY(guid)
+  if lastGUID == guid and inspectTarget then -- If this is the unit we requested information for,
+    inspectTarget = false
+    local name = UnitName("target")
+    local level = UnitLevel("target")
     local playerName = UnitName("player")
-    local localizedClass, englishClass, classIndex = UnitClass("mouseover")
+    local localizedClass, englishClass, classIndex = UnitClass("target")
     local realm = GetRealmName()
-    local guildName, guildRankName, guildRankIndex = GetGuildInfo("mouseover")
-    local race, raceFile, raceIndex = UnitRace("mouseover")
-    local gender = UnitSex("mouseover")
+    local guildName, guildRankName, guildRankIndex = GetGuildInfo("target")
+    local race, raceFile, raceIndex = UnitRace("target")
+    local gender = UnitSex("target")
     local date = date("%Y-%m-%d %H:%M:%S")
-      NotifyInspect("mouseover")
-      local itemString = ""
+    local itemString = ""
+    local enchantString = ""
+    local enchantSlots = {1, 3, 5, 7, 8, 9, 10, 15, 16, 17, 18}
       for i = 1, 19 do
-        local id = GetInventoryItemID("mouseover", i)
+        local id = GetInventoryItemID("target", i)
+
+        for j = 1, #enchantSlots do
+          if i == enchantSlots[j] then
+            local itemLink = GetInventoryItemLink("target", i)
+            local itemLinkString = tostring(itemLink)
+            local _, itemId, enchantId = strsplit(":", itemLinkString)
+            if enchantId == "" or enchantId == nil then
+              enchantString = enchantString .. "," .. "nil"
+            else
+              enchantString = enchantString .. "," .. enchantId
+            end
+          end
+        end
         id = tostring(id)
         itemString = itemString .. "," .. id
       end
@@ -41,25 +79,33 @@ f:SetScript("OnEvent", function (_, event, args)
             end
         end
       end
-      local formatted = format('%s,%s,%s,%s,%s,%s,%s,%s,%s,%s%s', playerName or "nil", date or "nil", realm or "nil", name or "nil", guildName or "nil", guildRankName or "nil", level or "nil", classIndex or 'nil', raceIndex or "nil", gender or "nil",  itemString or 'nil')
-
-    if #pattez_armory >= 500 and scanned == 50 then
-      print("[pattez_armory] has scanned too many players. Close all WoW clients and upload.")
-      scanned = 0
+      local formatted = format('%s,%s,%s,%s,%s,%s,%s,%s,%s,%s%s%s', playerName or "nil", date or "nil", realm or "nil", name or "nil", guildName or "nil", guildRankName or "nil", level or "nil", classIndex or 'nil', raceIndex or "nil", gender or "nil",  itemString or 'nil', enchantString or 'nil')
+    if #pattez_armory >= 500 then
+      if scanned == 50 then
+        print("[pattez_armory] has scanned too many players. Close all WoW clients and upload.")
+        scanned = 0
+      end
     elseif index > 0 and playerIndex == 0 then
-        pattez_armory[index] = formatted
+      print("[pattez_armory]:", "Sucessfully updated: " .. name)
+      pattez_armory[index] = formatted
     elseif playerIndex > 0 then
+      print("[pattez_armory]:", "Sucessfully updated: " .. name)
         pattez_armory[playerIndex] = formatted
     else
+        print("[pattez_armory]:", "Sucessfully inspected: " .. name)
         tinsert(pattez_armory, formatted)
-    end
+      end
+    tryAgain = false
+	end
+end
 
-      ClearInspectPlayer()
-    end
-  end
-end)
+function f:PLAYER_TARGET_CHANGED(guid)
+  inspectTarget = true
+  tryAgain = true
+  NotifyInspect("target")
+  MyAddOn:ScheduleTimer("TimerFeedback", 0.5)
+end
 
 f:RegisterEvent("INSPECT_READY")
 f:RegisterEvent("PLAYER_TARGET_CHANGED")
-f:RegisterEvent("UPDATE_MOUSEOVER_UNIT")
-f:RegisterEvent("ADDON_LOADED")
+
